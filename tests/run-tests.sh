67dc9fbf1972dcc7f8e638de107d2c1ad52a6ab4
James Hughes <jhughes@sci.utah.edu>
2014-01-28 20:19:20 -0700

    Rollback run-tests.sh.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -10,15 +10,9 @@ set -e
 
 pushd ./bin > /dev/null
   if [[ ! "-n" == $1 ]]; then
-    cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OS_MESA=ON -D CMAKE_EXPORT_COMPILE_COMMANDS="ON" ..
+    cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OS_MESA=ON ..
   fi
   make -j4
   ./gl_shader_tests
 popd
 
-PROJECT_BASE_DIR=${HOME}/me/games/leaf
-PROJECT_BIN_DIR=${PROJECT_BASE_DIR}/build/bin
-
-source ${HOME}/prosp/common/common.sh
-MergeCompileCommands $PROJECT_BIN_DIR
-ConstructYCMFile $PROJECT_BASE_DIR $PROJECT_BIN_DIR
